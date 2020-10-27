a4bee0216ac86fde8120812b76d0953f7111cf76
virsail <virsaileric@gmail.com>
2020-10-27 11:18:00 +0300

    define layout of the application

--- insta/forms.py
+++ insta/forms.py
@@ -1,10 +1,9 @@
 from django.forms import ModelForm
-#from .models import Event 
-#from django.contrib.auth.forms import UserCreationForm
 from django.contrib .auth.models import User
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from pyuploadcare.dj.forms import ImageField
+from .models import Profile, Post, Comment
 
 
 
