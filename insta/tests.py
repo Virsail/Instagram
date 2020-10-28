e9e8f2183e579ddb3fa35a5c777b7c3f19a8f3c0
virsail <virsaileric@gmail.com>
2020-10-28 23:20:29 +0300

    define the test cases in test.py

--- insta/tests.py
+++ insta/tests.py
@@ -1,70 +1,63 @@
 from django.test import TestCase
 from .models import Profile, Image, Comment 
+from django.contrib.auth.models import User
 # Create your tests here.
 
-class EventOrganizerTestClass(TestCase):
-    
-    # Set up method
+class TestProfile(TestCase):
     def setUp(self):
-        self.virsail= EventOrganizer(first_name = 'virsail', last_name ='Mbagaya', email ='virsaileric@gmail.com')
-    # Testing  instance
-    def test_instance(self):
-        self.assertTrue(isinstance(self.virsail,EventOrganizer))
-    # Testing Save Method
-    def test_save_method(self):
-        self.virsail.save_eventorganizer()
-        eventorganizers = EventOrganizer.objects.all()
-        self.assertTrue(len(eventorganizers) > 0)
+        self.user = User(username='Virsail')
+        self.user.save()
 
-
-class EventTestClass(TestCase):
-    
-    def setUp(self):
-        # Creating a new eventorganizer and saving it
-        self.virsail= EventOrganizer(first_name = 'Virsail', last_name ='Mbagaya', email ='virsaileric@gmail.com')
-        self.virsail.save_eventorganizer()
-
-        # Creating a new ticket and saving it
-        self.new_ticket = tickets(name = 'ticketing')
-        self.new_ticket.save()
-
-        #self.new_event= Event(title = '',post = '',eventorganizer = self.virsail)
-        #self.new_event.save()
-
-        #self.new_event.tickets.add(self.new_ticket)
-
-    def tearDown(self):
-        EventOrganizer.objects.all().delete()
-        tickets.objects.all().delete()
-        Event.objects.all().delete()
-
-    def test_get_event_today(self):
-        today_event = Event.todays_event()
-        self.assertFalse(len(today_event)>0)
-    
-    def test_get_event_by_date(self):
-        test_date = '2020-10-20'
-        date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
-        event_by_date = Event.days_event(date)
-        self.assertTrue(len(event_by_date) == 0)
-
-class CategoryTestClass(TestCase):
-    def setUp(self):
-        self.category = Category(name='colorfest254')
-        self.category.save_category()
+        self.profile_test = Profile(id=1, name='mode', profile_picture='grid.png', bio='reach out whenever you feel like things rea getting out of hand',
+                                    user=self.user)
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.category, Category))
+        self.assertTrue(isinstance(self.profile_test, Profile))
 
-    def test_save_category(self):
-        self.category.save_category()
-        categories = Category.objects.all()
-        self.assertTrue(len(categories) > 0)
+    def test_save_profile(self):
+        self.profile_test.save_profile()
+        after = Profile.objects.all()
+        self.assertTrue(len(after) > 0)
 
-    def test_delete_category(self):
-        self.category.delete_category()
-        category = Category.objects.all()
-        self.assertTrue(len(category) == 0)
 
+class TestImage(TestCase):
+    def setUp(self):
+        self.profile_test = Profile(name='virsail')
+        self.profile_test.save()
+
+        self.image_test = Image(image='grid.png', name='savage', caption='The guy just thought we are stupid', user=self.profile_test)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image_test, Image))
+
+    def test_save_image(self):
+        self.image_test.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def test_delete_image(self):
+        self.image_test.delete_image()
+        after = Profile.objects.all()
+        self.assertTrue(len(after) < 1)
+
+class TestComment(TestCase):
+    def setUp(self):
+        self.profile_test = Profile(name='virsail')
+        self.profile_test.save()
+
+        self.comment_test = Comment(image='grid.png', poster='virsail', comment='I like this shit')
+
+        def test_instance(self):
+        self.assertTrue(isinstance(self.comment_test, Comment))
+
+    def test_save_comment(self):
+        self.comment_test.save_comment()
+        comments = Comment.objects.all()
+        self.assertTrue(len(comments) > 0)
+
+    def test_delete_comment(self):
+        self.comment_test.delete_comment()
+        after = Profile.objects.all()
+        self.assertTrue(len(after) < 1)
         
     
